886e80dbcf188bd1bb2c99eadffad3f019254b59
ericvmai <30978780+ericvmai@users.noreply.github.com>
2023-09-22 19:21:53 -0400

    fixing some of my errors

--- r/hud/hcv.R
+++ r/hud/hcv.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(sf)
 
-hcv <- st_read("https://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/Housing_Choice_Vouchers_by_Tract/FeatureServer/4/query?f=json&where=(COUNTY%20IN%20('680'))%20AND%20(STATE%20IN%20('51'))&outFields=*", quiet = TRUE)
+hcv <- st_read("https://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/Housing_Choice_Vouchers_by_Tract/FeatureServer/4/query?f=json&where=(STATE%20IN%20('51'))%20AND%20(COUNTY%20IN%20('680'%2C%20'009'%2C%20'011'%2C%20'019'%2C%20'031'))&outFields=*", quiet = TRUE)
 
 write_rds(hcv, "data/hcv.rds")
