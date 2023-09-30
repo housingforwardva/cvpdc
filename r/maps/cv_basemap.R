6bd75f23dcd011c77bd60e9bb3a497f1133b1a64
jtknopf <jonathan@hdadvisors.net>
2023-09-30 19:43:09 -0400

    full pdf render

--- r/maps/cv_basemap.R
+++ r/maps/cv_basemap.R
@@ -10,6 +10,8 @@ lb <- "51680"
 
 cnty_code <- c("009", "011", "019", "031", "515", "680")
 
+lb_hex_count <- read_rds("data/lb_hex_count.rds")
+
 sf_use_s2(TRUE)
 
 options(tigris_use_cache = TRUE)
@@ -32,9 +34,17 @@ lb_basemap <- layer_static_mapbox(
   username = "mapbox"
 )
 
+lb_hex_basemap <- layer_static_mapbox(
+  location = lb_hex_count,
+  style_id = "light-v10",
+  username = "mapbox"
+)
+
 write_rds(cv_basemap, "data/cv_basemap.rds")
 
 write_rds(lb_basemap, "data/lb_basemap.rds")
+
+write_rds(lb_hex_basemap, "data/lb_hex_basemap.rds")
 
 write_rds(cv_boundary, "data/cv_boundary.rds")
 
